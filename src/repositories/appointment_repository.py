863dbf423170193ffb49877339554da32ed95d56
Vu14082002 <nguyenvanvu20020814@gmail.com>
2024-09-08 10:04:26 +0700

    update: updae logic ordered

--- src/repositories/appointment_repository.py
+++ src/repositories/appointment_repository.py
@@ -1,8 +1,11 @@
-import re
 
+import logging as log
+from datetime import datetime
+
+from attr import s
 from sqlalchemy import (Result, Row, and_, asc, case, delete, desc, exists,
                         func, or_, select)
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import joinedload, selectinload
 from tomlkit import aot
 
 from src.core.database.postgresql import PostgresRepository
@@ -20,13 +23,20 @@ class AppointmentRepository(PostgresRepository[AppointmentModel]):
         try:
             patient_model = await self._get_patient_model_by_id(patient_id)
             doctor_model = await self._get_doctor_model_by_id(doctor_id)
-            work_schedule_model = await self._get_work_schedule_model_by_id(
+            work_schedule_model = self._get_work_schedule_model_by_id(
                 work_schedule_id, doctor_model)
+            if work_schedule_model.ordered:
+                raise BadRequest(error_code=ErrorCode.ALLREADY_ORDERED.name, errors={
+                    "message": "Work schedule is ordered"})
             fee_examprice = self._get_fee_examination(
                 doctor_model, work_schedule_model)
+            # validate appointment
             appointment = AppointmentModel()
-            appointment.appointment_date_start = work_schedule_model.start_time
-            appointment.appointment_date_end = work_schedule_model.end_time
+            appointment.appointment_date_start = datetime.combine(
+                work_schedule_model.work_date, work_schedule_model.start_time)
+
+            appointment.appointment_date_end = datetime.combine(
+                work_schedule_model.work_date, work_schedule_model.end_time)
             appointment.patient = patient_model
             appointment.doctor = work_schedule_model.doctor
             appointment.appointment_status = "approved" if work_schedule_model.examination_type.lower(
@@ -34,28 +44,38 @@ class AppointmentRepository(PostgresRepository[AppointmentModel]):
             appointment.examination_type = work_schedule_model.examination_type
             appointment.pre_examination_notes = pre_examination_notes if pre_examination_notes else ""
             appointment.total_amount = fee_examprice
+            # set work schedule is ordered
+            work_schedule_model.ordered = True
+            # add appointment and work schedule to session
             self.session.add(appointment)
+            # self.session.add(work_schedule_model)
+            await self.session.flush()
             await self.session.commit()
             return {"message": "Create appointment successfully"}
         except (BadRequest) as e:
             raise e
         except Exception as e:
+            log.error(e)
             raise e
 
     async def _get_doctor_model_by_id(self, doctor_id: int):
         try:
             query = select(DoctorModel).where(DoctorModel.id == doctor_id).options(
-                selectinload(DoctorModel.examination_prices))
+                joinedload(DoctorModel.working_schedules),
+                joinedload(DoctorModel.examination_prices)
+            )
             result = await self.session.execute(query)
-            doctor_model = result.scalar_one_or_none()
+            doctor_model = result.unique().scalar_one_or_none()
 
             if doctor_model is None:
                 raise BadRequest(error_code=ErrorCode.NOT_FOUND.name, errors={
                     "message": "Doctor not found"})
             return doctor_model
         except BadRequest as e:
+            log.error(e)
             raise e
         except Exception as e:
+            log.error(e)
             raise e
 
     async def _get_patient_model_by_id(self, patient_id: int):
@@ -73,7 +93,7 @@ class AppointmentRepository(PostgresRepository[AppointmentModel]):
         except Exception as e:
             raise e
 
-    async def _get_work_schedule_model_by_id(self, work_scheduling_id: int, doctor: DoctorModel):
+    def _get_work_schedule_model_by_id(self, work_scheduling_id: int, doctor: DoctorModel):
         try:
             for item in doctor.working_schedules:
                 if item.id == work_scheduling_id:
@@ -82,8 +102,10 @@ class AppointmentRepository(PostgresRepository[AppointmentModel]):
             raise BadRequest(error_code=ErrorCode.NOT_FOUND.name, errors={
                 "message": "Work schedule not found"})
         except BadRequest as e:
+            log.error(e)
             raise e
         except Exception as e:
+            log.error(e)
             raise e
 
     def _get_fee_examination(self, doctor: DoctorModel, work_scheduling: WorkScheduleModel):
