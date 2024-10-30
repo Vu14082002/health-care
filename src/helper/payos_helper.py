ba0a511edbb6a84fd820c2aee0009a16cd338bdd
Vu14082002 <nguyenvanvu20020814@gmail.com>
2024-10-30 22:54:41 +0700

    fix: remove comment payos to test

--- src/helper/payos_helper.py
+++ src/helper/payos_helper.py
@@ -21,24 +21,22 @@ class PaymentHelper:
         time_session:int = 300
     ):
         try:
-            # utc_plus_7 = pytz.timezone("Asia/Ho_Chi_Minh")
-            # current_time_utc = datetime.now(pytz.utc)
-            # current_time_utc_plus_7 = current_time_utc.astimezone(utc_plus_7)
-            # time_expired = current_time_utc_plus_7 + timedelta(seconds=time_session)
-            # expired_at = int(time_expired.timestamp())
-            # random_number= random.randint(1000, 99999)
-            # payment_data = PaymentData(
-            #     orderCode=random_number,
-            #     amount=amount,
-            #     description=description,
-            #     cancelUrl=cancelUrl,
-            #     returnUrl=returnUrl,
-            #     expiredAt=expired_at,
-            # )
-            # payOsCreatePayment = payOsIns.createPaymentLink( payment_data)
-            # payOsCreatePayment = payOsCreatePayment.to_json()
-            # return payOsCreatePayment
-            return "xinchao"
+            utc_plus_7 = pytz.timezone("Asia/Ho_Chi_Minh")
+            current_time_utc = datetime.now(pytz.utc)
+            current_time_utc_plus_7 = current_time_utc.astimezone(utc_plus_7)
+            time_expired = current_time_utc_plus_7 + timedelta(seconds=time_session)
+            expired_at = int(time_expired.timestamp())
+            random_number= random.randint(1000, 99999)
+            payment_data = PaymentData(
+                orderCode=random_number,
+                amount=amount,
+                description=description,
+                cancelUrl=cancelUrl,
+                returnUrl=returnUrl,
+                expiredAt=expired_at,
+            )
+            payos_create_payment = payOsIns.createPaymentLink(payment_data)
+            return payos_create_payment.to_json()
         except Exception as e:
             print(e)
             return None
@@ -48,8 +46,8 @@ class PaymentHelper:
         orderId: str
     ):
         try:
-            paymentLinkInfo: PaymentLinkInformation = payOsIns.getPaymentLinkInformation(orderId = orderId)
-            return paymentLinkInfo.to_json()
+            payment_link_info: PaymentLinkInformation = payOsIns.getPaymentLinkInformation(orderId = orderId)
+            return payment_link_info.to_json()
 
         except Exception as e:
             print(e)
