7d07c5c3e5361bfb4d907688c4cf810ee1dd94c0
Vu14082002 <nguyenvanvu20020814@gmail.com>
2024-11-30 01:46:15 +0700

    feat: add retry for payos

--- src/helper/payos_helper.py
+++ src/helper/payos_helper.py
@@ -1,5 +1,6 @@
 import random
 from datetime import datetime, timedelta
+from typing import Final
 
 import pytz
 from payos import PaymentData
@@ -17,30 +18,35 @@ class PaymentHelper:
         amount: int,
         description: str,
         returnUrl: str,
-        cancelUrl:str = "",
+        cancelUrl:str|None = "",
         time_session:int = 300
     ):
-        try:
-            utc_plus_7 = pytz.timezone("Asia/Ho_Chi_Minh")
-            current_time_utc = datetime.now(pytz.utc)
-            current_time_utc_plus_7 = current_time_utc.astimezone(utc_plus_7)
-            time_expired = current_time_utc_plus_7 + timedelta(seconds=time_session)
-            expired_at = int(time_expired.timestamp())
-            random_number= random.randint(1000, 99999)
-            payment_data = PaymentData(
-                orderCode=random_number,
-                amount=amount,
-                description=description,
-                cancelUrl=cancelUrl,
-                returnUrl=returnUrl,
-                expiredAt=expired_at,
-            )
-            payos_create_payment = payOsIns.createPaymentLink(payment_data)
-            return payos_create_payment.to_json()
-        except Exception as e:
-            print(e)
-            return None
-
+        MAX_RETRY:Final[int]=5
+        retry_count=0
+        while retry_count < MAX_RETRY:
+            try:
+                utc_plus_7 = pytz.timezone("Asia/Ho_Chi_Minh")
+                current_time_utc = datetime.now(pytz.utc)
+                current_time_utc_plus_7 = current_time_utc.astimezone(utc_plus_7)
+                time_expired = current_time_utc_plus_7 + timedelta(seconds=time_session)
+                expired_at = int(time_expired.timestamp())
+                random_number= random.randint(1000, 99999)
+                payment_data = PaymentData(
+                    orderCode=random_number,
+                    amount=amount,
+                    description=description,
+                    cancelUrl=cancelUrl,
+                    returnUrl=returnUrl,
+                    expiredAt=expired_at,
+                )
+                payos_create_payment = payOsIns.createPaymentLink(payment_data)
+                return payos_create_payment.to_json()
+            except Exception as e:
+                retry_count += 1
+                print(f"Error occurred: {e}. Retry {retry_count}/{MAX_RETRY}")
+                if retry_count == MAX_RETRY:
+                    print("Max retry reached")
+                    return None
     def get_payment_info(
         self,
         orderId: str
