a98ce1efd8a40a51f755b6b874fde66aa0d24694
Vu14082002 <nguyenvanvu20020814@gmail.com>
2024-12-01 22:43:40 +0700

    feat: update logic admin all doctor

--- src/apis/docter_api.py
+++ src/apis/docter_api.py
@@ -6,10 +6,12 @@ from src.core.security import JsonWebToken
 from src.enum import ErrorCode, Role
 from src.factory import Factory
 from src.helper.doctor_helper import DoctorHelper
+from src.models.doctor_model import DoctorModel
 from src.schema.doctor_schema import (
     RequestDetailDoctorSchema,
     RequestDoctorPatientByIdSchema,
     RequestDoctorPatientSchema,
+    RequestGetAllDoctorsRootSchema,
     RequestGetAllDoctorsSchema,
 )
 
@@ -59,26 +61,35 @@ class GetAllDoctorApi(HTTPEndpoint):
 
 
 class GetAllDoctorLocalAPi(HTTPEndpoint):
-    async def get(self, query_params: RequestGetAllDoctorsSchema, auth: JsonWebToken):
+    async def get(self, query_params: RequestGetAllDoctorsRootSchema, auth: JsonWebToken):
         try:
+            user_role = auth.get("role")
+            if user_role != Role.ADMIN.name:
+                raise Forbidden(
+                    error_code=ErrorCode.FORBIDDEN.name,
+                    errors={
+                        "message": ErrorCode.msg_permission_denied.value,
+                    },
+                )
             doctor_helper: DoctorHelper = await Factory().get_doctor_helper()
-            current_page = query_params.current_page if query_params.current_page else 0
-            page_size = query_params.page_size if query_params.page_size else 10
-            where = {"verify_status": {"$eq": 2}, "is_local_person": True}
-            if query_params.type_of_disease:
-                if query_params.type_of_disease == "both":
-                    where["$or"] = [
-                        {"type_of_disease": "online"},
-                        {"type_of_disease": "offline"},
-                        {"type_of_disease": "both"},
-                    ]
-                else:
-                    where["type_of_disease"] = query_params.type_of_disease
-            response_data = await doctor_helper.get_all_doctor(
-                current_page=current_page,
-                page_size=page_size,
-                where=where,
-                text_search=query_params.text_search,
+
+            _current_page = query_params.current_page
+            _page_size = query_params.page_size
+            _filter = {
+                k: v
+                for k, v in query_params.model_dump().items()
+                if v is not None and hasattr(DoctorModel, k)
+            }
+            _filter["is_local_person"] = True
+            _filter["verify_status"] = 2
+            _text_search = query_params.text_search
+            response_data = await doctor_helper.get_all_doctor_local(
+                current_page=_current_page,
+                page_size=_page_size,
+                text_search=_text_search,
+                filter_data=_filter,
+                order_by=query_params.order_by,
+                is_desc=query_params.is_desc,
             )
             return response_data
         except Exception as e:
@@ -92,35 +103,35 @@ class GetAllDoctorLocalAPi(HTTPEndpoint):
 
 
 class GetAllDoctorForeignAPi(HTTPEndpoint):
-    async def get(self, query_params: RequestGetAllDoctorsSchema, auth: JsonWebToken):
+    async def get(self, query_params: RequestGetAllDoctorsRootSchema, auth: JsonWebToken):
         try:
-            if auth.get("role") != Role.ADMIN.name:
+            user_role = auth.get("role")
+            if user_role != Role.ADMIN.name:
                 raise Forbidden(
-                    msg="Permission denied",
                     error_code=ErrorCode.FORBIDDEN.name,
                     errors={
                         "message": ErrorCode.msg_permission_denied.value,
                     },
                 )
             doctor_helper: DoctorHelper = await Factory().get_doctor_helper()
-            current_page = query_params.current_page if query_params.current_page else 0
-            page_size = query_params.page_size if query_params.page_size else 10
-            where = {"verify_status": {"$eq": 2}, "is_local_person": False}
-            if query_params.type_of_disease:
-                if query_params.type_of_disease == "both":
-                    where["$or"] = [
-                        {"type_of_disease": "online"},
-                        {"type_of_disease": "offline"},
-                        {"type_of_disease": "both"},
-                    ]
-                else:
-                    where["type_of_disease"] = query_params.type_of_disease
 
-            response_data = await doctor_helper.get_all_doctor(
-                current_page=current_page,
-                page_size=page_size,
-                where=where,
-                text_search=query_params.text_search,
+            _current_page = query_params.current_page
+            _page_size = query_params.page_size
+            _filter = {
+                k: v
+                for k, v in query_params.model_dump().items()
+                if v is not None and hasattr(DoctorModel, k)
+            }
+            _filter["is_local_person"] = False
+            _filter["verify_status"] = 2
+            _text_search = query_params.text_search
+            response_data = await doctor_helper.get_all_doctor_local(
+                current_page=_current_page,
+                page_size=_page_size,
+                text_search=_text_search,
+                filter_data=_filter,
+                order_by=query_params.order_by,
+                is_desc=query_params.is_desc,
             )
             return response_data
         except Exception as e:
